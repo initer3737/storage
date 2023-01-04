0d42c08c904dcf41af91df96ecc1c8003404d7dd
puthutwahyuaji <puthutwahyuaji13@gmail.com>
2023-01-04 19:22:38 +0700

    notification sample

---
+++ bootstrap/notification/src/js/script.js
@@ -0,0 +1,28 @@
+// 'use strict';
+// // import vika from '../img/vika.png'
+// if('Notification' in window){
+//    const showNotif=()=>{
+//         let title='comrade Vika need you!!'
+//         let body=`serve to the motherland!!! \n comrade ${localStorage.getItem('player')}\n war event ended 3 day left `
+//         let icon='src/img/vika.png'
+//         let noti=new Notification(title,{
+//             body,icon
+//         })
+//         noti.onclick=((e)=>{
+//             window.location.href='https://initer3737.github.io'
+//         })
+//         noti.close()
+//    }
+//         if(Notification.permission === 'granted'){
+//             showNotif()
+//         }else if(Notification.permission !== 'denied'){
+//                 Notification.requestPermission().then((perm)=>{
+//                     if(perm === 'granted'){
+//                         showNotif();
+//                     }
+//                 })
+//         }
+// }
+
+
+// localStorage.setItem('player','yotsusan')
