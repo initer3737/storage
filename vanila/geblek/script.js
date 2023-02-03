1e03dc8b73ea7439c25d952820d8d8dfcf003233
puthutwahyuaji <puthutwahyuaji13@gmail.com>
2023-02-03 18:57:31 +0700

    update

--- vanila/geblek/script.js
+++ vanila/geblek/script.js
@@ -4,3 +4,12 @@ getIdElement('icon').addEventListener('click',(e)=>{
     const attr=getIdElement('geblek').getAttribute('tes')
     alert(attr)
 })
+
+const body=document.getElementsByTagName("BODY")[0]
+
+ setInterval(jam,1000)
+
+        function jam(){
+            let jam=new Date()
+            getIdElement('jam').innerHTML=jam.toLocaleTimeString()
+        }
